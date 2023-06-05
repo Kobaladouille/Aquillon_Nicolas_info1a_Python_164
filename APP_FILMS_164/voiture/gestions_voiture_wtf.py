010cff2c2cb59ef82ca9c0e16fde4890b7500933
euwimy <nicolasaquillon@gmail.com>
2023-06-05 08:40:40 +0200

    first commit

---
+++ APP_FILMS_164/voiture/gestions_voiture_wtf.py
@@ -0,0 +1,75 @@
+"""
+    Fichier : gestions_voiture_wtf.py
+    Auteur : OM 2021.03.22
+    Gestion des formulaires avec WTF
+"""
+from flask_wtf import FlaskForm
+from wtforms import StringField, DateField
+from wtforms import SubmitField
+from wtforms.validators import Length, InputRequired, DataRequired
+from wtforms.validators import Regexp
+
+
+class FormWTFAjouterGenres(FlaskForm):
+    """
+        Dans le formulaire "voiture_ajouter_wtf.html" on impose que le champ soit rempli.
+        Définition d'un "bouton" submit avec un libellé personnalisé.
+    """
+    nom_genre_regexp = "^([A-Z]|[a-zÀ-ÖØ-öø-ÿ])[A-Za-zÀ-ÖØ-öø-ÿ]*['\- ]?[A-Za-zÀ-ÖØ-öø-ÿ]+$"
+    nom_chevaux_regexp = "^(. *[^0-9]|)(1000|[1-9]\d{0,2})([^0-9]. *|)$"
+    nom_genre_wtf = StringField("entrez la Marque de la voiture", validators=[Length(min=2, max=20, message="min 2 max 20"),
+                                                                   Regexp(nom_genre_regexp,
+                                                                          message="Pas de chiffres, de caractères "
+                                                                                  "spéciaux, "
+                                                                                  "d'espace à double, de double "
+                                                                                  "apostrophe, de double trait union")
+                                                                   ])
+    nom_modele_wtf = StringField("entrez le modèle de la ", validators=[Length(min=2, max=20, message="min 2 max 20"),
+                                                                   Regexp(nom_genre_regexp,
+                                                                          message="Pas de chiffres, de caractères "
+                                                                                  "spéciaux, "
+                                                                                  "d'espace à double, de double "
+                                                                                  "apostrophe, de double trait union")
+                                                                   ])
+    nom_chevaux_wtf = StringField("entrez le nombre de chevaux de la voiture", validators=[Length(min=2, max=20, message="min 2 max 20"),
+                                                                   Regexp(nom_chevaux_regexp,
+                                                                          message="Seulement les chiffres / Nombres sont accépter "
+                                                                                )
+                                                                   ])
+
+    submit = SubmitField("Enregistrer la voiture")
+
+
+class FormWTFUpdateGenre(FlaskForm):
+    """
+        Dans le formulaire "voiture_update_wtf.html" on impose que le champ soit rempli.
+        Définition d'un "bouton" submit avec un libellé personnalisé.
+    """
+    nom_genre_update_regexp = "^([A-Z]|[a-zÀ-ÖØ-öø-ÿ])[A-Za-zÀ-ÖØ-öø-ÿ]*['\- ]?[A-Za-zÀ-ÖØ-öø-ÿ]+$"
+    nom_genre_update_wtf = StringField("Clavioter le genre ", validators=[Length(min=2, max=20, message="min 2 max 20"),
+                                                                          Regexp(nom_genre_update_regexp,
+                                                                                 message="Pas de chiffres, de "
+                                                                                         "caractères "
+                                                                                         "spéciaux, "
+                                                                                         "d'espace à double, de double "
+                                                                                         "apostrophe, de double trait "
+                                                                                         "union")
+                                                                          ])
+    date_genre_wtf_essai = DateField("Essai date", validators=[InputRequired("Date obligatoire"),
+                                                               DataRequired("Date non valide")])
+    submit = SubmitField("Update genre")
+
+
+class FormWTFDeleteGenre(FlaskForm):
+    """
+        Dans le formulaire "voiture_delete_wtf.html"
+
+        nom_genre_delete_wtf : Champ qui reçoit la valeur du genre, lecture seule. (readonly=true)
+        submit_btn_del : Bouton d'effacement "DEFINITIF".
+        submit_btn_conf_del : Bouton de confirmation pour effacer un "genre".
+        submit_btn_annuler : Bouton qui permet d'afficher la table "t_genre".
+    """
+    nom_genre_delete_wtf = StringField("Effacer ce genre")
+    submit_btn_del = SubmitField("Effacer genre")
+    submit_btn_conf_del = SubmitField("Etes-vous sur d'effacer ?")
+    submit_btn_annuler = SubmitField("Annuler")
